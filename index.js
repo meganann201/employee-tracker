b21f3806fb4d0df842eb669b93e83d3b316a539d
Megan Campbell <megancampbell201@gmail.com>
2022-03-16 13:27:15 -0400

    added inquirer prompt function for user input

--- index.js
+++ index.js
@@ -1,3 +1,67 @@
 const inquirer = require("inquirer");
 const db = require("./db/connection.js");
 const cTable = require("console.table");
+async function toDo() {
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "toDo",
+        message: "What would you like to do?",
+        choices: [
+          "View all departments",
+          "View all roles",
+          "View all employees",
+          "Add a department",
+          "Add a role",
+          "Add an employee",
+          "Update an employee role",
+          "Delete an employee",
+          "Want to exit",
+        ],
+      },
+    ])
+    .then((answers) => {
+      switch (answers.toDo) {
+        case "View all departments":
+          selectAllDepartments().then(([rows, fields]) => {
+            console.table(rows);
+            wantToExit();
+          });
+          break;
+        case "View all roles":
+          selectAllRoles().then(([rows, fields]) => {
+            console.table(rows);
+            wantToExit();
+          });
+          break;
+        case "View all employees":
+          selectAllEmployees().then(([rows, fields]) => {
+            console.table(rows);
+            wantToExit();
+          });
+          break;
+        case "Add a department":
+          addDepartment();
+
+          break;
+        case "Add a role":
+          addRole();
+
+          break;
+        case "Add an employee":
+          addEmployee();
+
+          break;
+        case "Update an employee role":
+          updateEmployee();
+
+          break;
+        case "Delete an employee":
+          deleteEmployee();
+
+          break;
+        default:
+      }
+    });
+}
